e5e0bbdc553d9bdf45a7fd4fe69368cd48b44fb2
BuiDucTuyen <buiductuyengl1072002@gmail.com>
2024-04-26 10:13:56 +0700

    doenenen

--- src/Components/Frame/index.tsx
+++ src/Components/Frame/index.tsx
@@ -17,8 +17,14 @@ const Frame: React.FC = () => {
               className="flex-1 px-[32px] rounded-md py-[14px] outline-none"
             />
           </div>
-          <button className="bg-[#EC008C] text-white text-[16px] font-semibold rounded-lg w-28 h-14">
-            {t("send")}
+          <button className="bg-[#EC008C] text-white flex justify-center items-center text-[16px] font-semibold rounded-lg w-28 h-14">
+            <p className="hidden sm:block">{t("send")}</p>
+
+            <img
+              className="block sm:hidden"
+              src="./svg/bi_send-fill.svg"
+              alt=""
+            />
           </button>
         </div>
       </nav>
